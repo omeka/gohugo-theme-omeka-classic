5670ff1843f744f0f03d8bd3684077ac863bbd4f
Kim Nguyen <design@kimisgold.com>
2026-03-19 22:17:49 -0400

    Generalize nav tree js.

--- static/js/main.js
+++ static/js/main.js
@@ -5,26 +5,7 @@ if (!ThanksRoy) {
 (function ($) {
 
     ThanksRoy.mobileMenu = function() {
-        var subNavId = 1;
-        $('#primary-nav li ul').each(function() {
-            var childMenu = $(this);
-            var subnavToggle = $('<button type="button" class="sub-nav-toggle" aria-label="Toggle subnavigation" aria-expanded="false"></button>');
-            childMenu.parent().addClass('parent');
-            childMenu.attr('id', 'sub-nav-' + subNavId);
-            subnavToggle.attr('aria-controls', 'sub-nav-' + subNavId);
-            subNavId++;
-            childMenu.addClass('sub-nav').before(subnavToggle);
-        });
-
-        $('#primary-nav').on('click', '.sub-nav-toggle', function() {
-            var subnavToggle = $(this);
-            subnavToggle.parent('.parent').toggleClass('open');
-            if (subnavToggle.attr('aria-expanded', 'false')) {
-                subnavToggle.attr('aria-expanded', 'true');
-            } else {
-                subnavToggle.attr('aria-expanded', 'false');
-            }
-        });
+        ThanksRoy.navTree('primary-nav');
 
         $(document).on('click', '.menu-button', function(e) {
             e.preventDefault();
@@ -37,6 +18,35 @@ if (!ThanksRoy) {
                 toggle.attr('aria-expanded', 'false')
             }
         });
+    };
+
+    ThanksRoy.navTree = function(navId) {
+        let subNavId = 1;
+        let navElement = $('#' + navId);
+        navElement.find('li ul').each(function() {
+            let childMenu = $(this);
+            let subnavToggle = $('<button type="button" class="sub-nav-toggle" aria-label="Toggle subnavigation" aria-expanded="false"></button>');
+            childMenu.parent().addClass('parent');
+            childMenu.attr('id', 'sub-nav-' + subNavId);
+            subnavToggle.attr('aria-controls', navId + '-sub-nav-' + subNavId);
+            subNavId++;
+            childMenu.addClass('sub-nav').before(subnavToggle);
+        });
+
+        navElement.on('click', '.sub-nav-toggle', function() {
+            let subnavToggle = $(this);
+            subnavToggle.parent('.parent').toggleClass('open');
+            if (subnavToggle.attr('aria-expanded', 'false')) {
+                subnavToggle.attr('aria-expanded', 'true');
+            } else {
+                subnavToggle.attr('aria-expanded', 'false');
+            }
+        });
+
+        if (navElement.find('.active').length > 0) { 
+            let activeParent = navElement.find('.active').parents('.parent');
+            activeParent.find('.sub-nav-toggle').click();
+        }
     };
 
 })(jQuery);
